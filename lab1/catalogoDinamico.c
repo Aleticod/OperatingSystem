1db5958fca72ba2c02fa2d09afaba54d5a7b07bf
harveyk <harveyk@harveyk.harveyk>
2022-05-24 17:37:47 -0700

    La tarea 3 termiando

--- lab1/catalogoDinamico.c
+++ lab1/catalogoDinamico.c
@@ -1,6 +1,6 @@
-// Nombre			: busquedaAutor.c
-// Proposito		: Administra un catalogo de libros
-// Autores			: A. Harvey Pfoccori y Marjori
+// Nombre			: catalogoDinamico.c
+// Proposito		: Administra un catalogo de libros dinamico
+// Autores			: A. Harvey Pfoccori Quispe y Marjorie Rodriguez Casa
 // FCreacion		: 22/05/2022
 // FModif			: 24/05/2022
 
@@ -10,54 +10,120 @@
 #include <stdlib.h>
 
 
-// Definicion de la estructura tLibro para almacenar
+// Definicion de la estructura tLibro
 typedef struct tLibro {
-	char titulo[51]; // Array string  titulo de tamanio 50
-	char autor[51];  // Array string  autor de tamanio 50
-	struct tLibro *siguiente
+	char titulo[51]; // Declarar Array string titulo de tamanio 50
+	char autor[51];  // Declarar Array string autor de tamanio 50
+	struct tLibro *siguiente; // Creacion de un nodo (puntero) siguiente
 } tLibro;
 
-// Definicion de la cabecera de esta lista
+// Definicion de la cabecera de esta lista (primero elemento objeto en la lista)
 typedef struct {
 	tLibro *primero;
-} tListaLibro;
+} tListaLibros;
 
-// Declarar la lista de Libros
-tListaLibro *miLista = NULL;
+// Declarar catalogo de Libros
+tListaLibros *miCatalogo = NULL;
 
 // Leer datos de un nuevo libro
-void leerDatos(struct tLibro * libro) {
+void leerDatos() {
+	char tit[51];	//Declarar Array string para titulo
+	char aut[51];	//Declarar Array string para autor
+
+//Nota: La estructura %[^\n]%*c Indica que se recibira
+// un conjunto de caracteres (incluido espacio) hasta una nueva linea
+
 	printf("Ingrese el titulo (tam max 50 letras): ");
-	scanf("%[^\n]%*c", libro -> titulo);
-	fflush(stdin);
+	scanf("%[^\n]%*c", tit);	// Recibe el titulo por consola
+
 	printf("Ingrese el autor (tam max 50 letras): ");
-	scanf("%[^\n]%*c", libro -> autor);
-}
+	scanf("%[^\n]%*c", aut); 	// Recibe el autor por consola
 
-// Mostrar datos del libro (titulo y autor)
-void mostrarDatos(struct tLibro * libro) {
-	printf("Titulo: %s\t\tAutor: %s\n", libro -> titulo, libro -> autor);
-}
+	// Creacion del nodo "nuevo" que contendra los datos anteriores
+	tLibro *nuevo = (tLibro *) malloc(sizeof(tLibro)); //Recervamos una cantidad  de memoria para un nodo de estructura tLibro
+	strcpy(nuevo -> titulo, tit); // Uso de la libreria string.h para hacer copia
+	strcpy(nuevo -> autor, aut);  // Uso de la libreria string.h para pasar datos de tit a titulo
 
-// Buscar un libro por titulo
-bool buscarTitulo(struct tLibro c[], char criterio[]) {
-	int i;
-	for(i = 0; i < tam; i++) {
-		if(strcmp(c[i].titulo, criterio) == 0)
-			return true;
+	// Campo que apunta al siguiente nodo es un nulo
+	nuevo -> siguiente = NULL;
+
+	// Llenado de datos al catalogo
+	if(miCatalogo -> primero == NULL) {
+		miCatalogo -> primero = nuevo; // Se anade nuevo como primer elemento
 	}
-	return false;
+	else {
+		tLibro *aux = miCatalogo -> primero; // Se crea campo aux que recorre el catalogo a partir del primero libro
+		while(aux -> siguiente != NULL) {
+			aux = aux -> siguiente;
+		}
+
+		aux -> siguiente = nuevo;	// Cuando el campo siguiente de aux es nulo, entonces ahora siguiente apuntara a nuevo
+	}
+}
+
+// Listar los libros del catalogo
+void listarCatalogo() {
+	if(miCatalogo -> primero == NULL)	// Si el catalogo esta vacia
+		printf("El catalogo esta vacio");
+	else {
+		tLibro *aux = miCatalogo -> primero; // Se crea un campo aux que recorra los elementos del catalogo
+		do {
+			printf("Titulo: %s\t\tAutor: %s\n", aux -> titulo, aux -> autor);
+			aux = aux -> siguiente;
+		}while(aux != NULL);
+	}
+}
+
+// Bucar por titulo
+void buscarTitulo() {
+	// Declaracion de la variable que almacenara el dato a buscar
+	char datos[51];
+	printf("Ingrese el titulo del texto (tam max 50 letras): ");
+	scanf("%[^\n]%*c", datos);  // Ingreso de datos por consola
+
+	if(miCatalogo -> primero == NULL)
+		printf("La lista esta vacia\n");
+	else {
+		tLibro *aux = miCatalogo -> primero;  // Creacion de un campo auxiliar que recorre los elementos del catalogo
+		do {
+			if(strcmp(aux -> titulo, datos) == 0) {  // 0 = son iguales, 1 = son diferentes
+				printf("\nUrraaaa!!! El titulo existe en la biblioteca\n\n");
+				break;
+			}
+			else {
+				aux = aux -> siguiente;
+				if (aux == NULL)
+					printf("\nUpppss!!! El titulo no existe en la biblioteca\n\n");
+			}
+		}while(aux != NULL);
+	}
 }
 
 // Buscar un libro por autor
-bool buscarAutor(struct tLibro c[], char criterio[]) {
-	int i;
-	for(i = 0; i < tam; i++) {
-		if(strcmp(c[i].autor,criterio) ==0)
-			return true;
+void buscarAutor() {
+	char datos[51];
+
+	if(miCatalogo -> primero == NULL)
+		printf("\nSorry!!! La lista esta vacia\n");
+	else {
+		printf("Ingrese el autor del texto (tam max 50 letras): ");
+		scanf("%[^\n]%*c", datos);
+
+		tLibro *aux = miCatalogo -> primero;
+		do {
+			if(strcmp(aux -> autor, datos) == 0) {
+				printf("\nUrraaaa!!! El autor existe en la biblioteca\n\n");
+				break;
+			}
+			else {
+				aux = aux -> siguiente;
+				if (aux == NULL)
+					printf("\nUpppss!!! El autor no existe en la biblioteca\n\n");
+			}
+		}while(aux != NULL);
 	}
-	return false;
 }
+
 
 // Menu
 void mostrarMenu() {
@@ -75,54 +141,41 @@ void mostrarMenu() {
 }
 
 int main () {
-	// Variables
-	struct tLibro catalogo[tam];
-	char datos[21];
-	char aux[2];
+	// Variable para recorrer la opcion menu
 	int opcion;
-	int i = 0;
-	int j;
+	char aux[2];
 
+	// Inicializar de nuestro catalogo de libros
+	miCatalogo = (tListaLibros *) malloc(sizeof(tListaLibros)); // Reservar memoria para un tipo tListaLibros
+	miCatalogo -> primero = NULL;  // Primer elemento inicia en nullo
+
+	// Mostar el menu, solicitar la opcion y acutar
 	do {
 		// Indicar que hacer al programa
 		mostrarMenu();
 
-		// Leer datos
+		// Leer opcion
 		printf("\nIngrese una opcion (1-5): ");
 		scanf("%[^\n]%*c", aux);
-		opcion = atoi(aux);
+		opcion = atoi(aux);  // Castear el tipo de dato char a numerico
+
+		// Evalucion de opciones
 		switch(opcion) {
 			case 1:
-				// Agregar titulo y autor
-				if(i < tam) {
-					leerDatos(&catalogo[i]);
-					i++;
-				}
+				// Leer datos de consola
+				leerDatos();
 				break;
 			case 2:
-				// Mostrar catalogo
-				for(j = 0; j < tam; j++)
-					mostrarDatos(&catalogo[j]);
+				// Mostrar catalogo en consola
+				listarCatalogo();
 				break;
 			case 3:
 				// Mostrar buscar por titulo
-				printf("Ingrese el titulo del texto (tam max 50 letras): ");
-				scanf("%[^\n]%*c", datos);
-				puts(datos);
-				if(buscarTitulo(catalogo,datos))
-					printf("\nUrraaaa!!! El titulo existe en la biblioteca\n\n");
-				else
-					printf("\nUppsss!!!! El titulo no existe en la biblioteca\n\n");
+				buscarTitulo();
 				break;
 			case 4:
 				// Mostrar buscar por autor
-				printf("Ingrese el autor del texto (tam max 50 letras): ");
-				scanf("%[^\n]%*c", datos);
-				puts(datos);
-				if(buscarAutor(catalogo,datos))
-					printf("\nUrraaaa!!!! El autor existe en la biblioteca\n\n");
-				else
-					printf("\nUppss!!!! El autor no existe en el biblioteca\n\n");
+				buscarAutor();
 				break;
 			case 5:
 				break;
