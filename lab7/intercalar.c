3ebbb0f629424f98c88290fadb96ce9143b61aa3
Harvey Unsaac <193837@unsaac.edu.pe>
2022-08-17 08:29:13 +0200

    The code of intercarlar.c was fixed

--- lab7/intercalar.c
+++ lab7/intercalar.c
@@ -2,6 +2,8 @@
 // Proposito	: Lee dos archivos de texto y crea un tercero con las líneas de los dos intercaladas.
 // Autor	    : Alex H. Pfoccori Quispe y Marjorie R. Rodriguez Casas
 // FCreacion	: 16/08/2022
+// Compilacion  : gcc intercalar.c -o intercalar.exe -Wall
+// Ejecucion    : ./intercalar.exe archivo1.txt archivo2.txt archResult.txt -lm
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,8 +17,10 @@
 
 int main(int argc,char **argv) 
 {
-    bool quien=true;
-    int i,j;
+    // Variables
+    int cont1 = 0;
+    int cont2 = 0;
+    int contT = 0;
     int fdo1, fdo2, fdd;
     char *or1;
     char *or2;
@@ -27,18 +31,21 @@ int main(int argc,char **argv)
     struct stat bstat1;
     struct stat bstat2;
 
+    // Verificar los argumentos
     if(argc != 4 )
     {
-        fprintf(stderr,"Uso: %s origen1 origen2\n", argv[0]);
+        fprintf(stderr,"Uso: %s origen1 origen2 destino\n", argv[0]);
         exit(1);
     }
 
+    // Abrir el primer archivo de origen
     if( (fdo1=open (argv[1], O_RDONLY))<0 )
     {
         perror("No se puede abrir el archivo de origen 1");
         exit(1);
     }
 
+    // Abrir el segundo archivo de origen
     if( (fdo2= open(argv[2], O_RDONLY))<0 )
     {
         perror("No se puede abrir el archivo de origen 2");
@@ -46,6 +53,7 @@ int main(int argc,char **argv)
         exit(1);
     }
 
+    // Crear el archivo de destino
     if( (fdd=open(argv[3], O_CREAT|O_TRUNC|O_RDWR)) <0)
     {
         perror("No se puede crear el archivo de destino");
@@ -53,6 +61,8 @@ int main(int argc,char **argv)
         close(fdo2);
         exit(1);
     }
+
+    // Determinar la longitud del primer archivo de origen
     if(fstat(fdo1, &bstat1)<0 )
     {
         perror("Error en fstat del archivo origen 1");
@@ -63,6 +73,7 @@ int main(int argc,char **argv)
         exit(1);
     }
 
+    // Determinar la longitud del segundo archivo de origen
     if(fstat(fdo2, &bstat2)<0 )
     {
         perror("Error en fstat del archivo origen 2");
@@ -73,6 +84,8 @@ int main(int argc,char **argv)
         exit(1);
     }
 
+    // Establecer que la longitud del archivo destino es igual
+    // a la suma de las longitudes de los archivos de origen
     if( ftruncate(fdd,bstat1.st_size + bstat2.st_size) < 0 ) 
     {
         perror("Error en ftruncate del archivo destino");
@@ -82,6 +95,8 @@ int main(int argc,char **argv)
         unlink(argv[3]);
         exit(1);
     }
+
+    // Se proyecta el primer archivo de origen
     if((or1=mmap((caddr_t)0,bstat1.st_size, PROT_READ,MAP_PRIVATE,fdo1,0))==MAP_FAILED)
     {
         perror("Error en la proyeccion del archivo de origen 1");
@@ -91,6 +106,8 @@ int main(int argc,char **argv)
         unlink(argv[3]);
         exit(1);
     }
+
+    // Se proyecta el segundo archivo de origen
     if((or2=mmap((caddr_t)0,bstat2.st_size, PROT_READ,MAP_PRIVATE,fdo2,0))==MAP_FAILED)
     {
         perror("Error en la proyeccion del archivo de origen 2");
@@ -100,6 +117,8 @@ int main(int argc,char **argv)
         unlink(argv[3]);
         exit(1);
     }
+
+    // Se proyecta el archivo de destino
     if((des=mmap((caddr_t)0,bstat1.st_size+bstat2.st_size, PROT_WRITE,MAP_SHARED,fdd,0))==MAP_FAILED)
     {
         perror("Error en la proyeccion del archivo de destino");
@@ -110,7 +129,7 @@ int main(int argc,char **argv)
         exit(1);
     }
 
-
+    // Se cierran los archivos
     close(fdo1);
     close(fdo2);
     close(fdd);
@@ -118,17 +137,29 @@ int main(int argc,char **argv)
     p1=or1;
     p2=or2;
     q=des;
-    int Aux= sqrt(bstat1.st_size + bstat2.st_size);
-    for(i=0;i<Aux;i++)
-    {
-        while(*p1 !='\n')
-            *q++ = *p1++;
-        *p1++;
-        *q++='\n';
-        while(*p2 !='\n')
-            *q++ = *p2++;
-        *p2++;
-        *q++='\n'; 
+
+    int Aux = bstat1.st_size + bstat2.st_size;
+    
+    while (contT < Aux)
+    {   if (cont1 < bstat1.st_size) {
+            while(*p1 !='\n') {
+                *q++ = *p1++;
+                cont1++;
+            }
+            
+            *q++=*p1++;
+            cont1++;
+        }
+        if (cont2 < bstat2.st_size) {
+            while(*p2 !='\n') {
+                *q++ = *p2++;
+                cont2++;
+            }
+            
+            *q++=*p2++;
+            cont2++;
+        }
+        contT = cont1 + cont2;
     }
 
     munmap(or1,bstat1.st_size);
